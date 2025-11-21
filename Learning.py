8746f28129ee2b3da8543acff728c5eb08fa667a
prathamitbawkar <prathamitbawkar@gmail.com>
2025-11-21 14:27:42 +0530

    defining class

--- Learning.py
+++ Learning.py
@@ -1,6 +1,7 @@
-def even_numbers(self):
-    for i in range(1,10):
-        if i%2==0:
-            print("even")
-        else:
-            print("odd")
+class Numbers:
+    def even_numbers(self):
+        for i in range(1,10):
+            if i%2==0:
+                print("even")
+            else:
+                print("odd")
